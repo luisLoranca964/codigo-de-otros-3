1f2d96851feff880c634dc021f29c9cc33f33008
luisLoranca964 <loranca964@gmail.com>
2024-09-27 17:10:53 -0600

    Se concluye con el ejercicio

--- filtro.js
+++ filtro.js
@@ -1,59 +1,63 @@
 // Tenemos un li de productos
 
 const productos = [
-  {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
-  {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
-  {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
-  {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
-  {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
+  { nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg" },
+  { nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg" },
+  { nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg" },
+  { nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg" },
+  { nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg" }
 ]
+//se cambia a getElementById porque el otro era por nombre de la etiqueta html y era plural
+const li = document.getElementById("lista-de-productos")
+//se quita simbolo $ a la variable, el punto al input ya que no es una clase y una 
+//variable no puede comenzar con caracter especial 
+const i = document.querySelector('input');
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
+//Se agrega funcion displayProductos ya que aqui es don se crea una etiqueta div y se comienza a agregar los productos
+function displayProductos() {
+  for (let i = 0; i < productos.length; i++) {
+    var d = document.createElement("div")
+    d.classList.add("producto")
 
-for (let i = 0; i < productos.length; i++) {
-  var d = document.createElement("div")
-  d.classList.add("producto")
+    var ti = document.createElement("p")
+    ti.classList.add("titulo")
+    ti.textContent = productos[i].nombre
 
-  var ti = document.createElement("p")
-  ti.classList.add("titulo")
-  ti.textContent = productos[i].nombre
-  
-  var imagen = document.createElement("img");
-  imagen.setAttribute('src', productos[i].img);
+    var imagen = document.createElement("img");
+    imagen.setAttribute('src', productos[i].img);
 
-  d.appendChild(ti)
-  d.appendChild(imagen)
+    d.appendChild(ti)
+    d.appendChild(imagen)
 
-  li.appendChild(d)
+    li.appendChild(d)
+  }
 }
-
 displayProductos(productos)
 const botonDeFiltro = document.querySelector("button");
 
-botonDeFiltro.onclick = function() {
+botonDeFiltro.onclick = function () {
   while (li.firstChild) {
     li.removeChild(li.firstChild);
   }
-
-  const texto = $i.value;
+  //se quita simbolo $ a la variable porque una variable no puede comenzar con simbolo especial
+  const texto = i.value;
   console.log(texto);
-  const productosFiltrados = filtrado(productos, texto );
+  const productosFiltrados = filtrado(productos, texto);
 
   for (let i = 0; i < productosFiltrados.length; i++) {
     var d = document.createElement("div")
     d.classList.add("producto")
-  
+
     var ti = document.createElement("p")
     ti.classList.add("titulo")
     ti.textContent = productosFiltrados[i].nombre
-    
+
     var imagen = document.createElement("img");
     imagen.setAttribute('src', productosFiltrados[i].img);
-  
+
     d.appendChild(ti)
     d.appendChild(imagen)
-  
+
     li.appendChild(d)
   }
 }
